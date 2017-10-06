c08cc6fc843eb60201c146962e4e76fc1659f640
thatMeow <thatMeow@users.noreply.github.com>
2017-10-05 21:49:13 -0400

    Update feature_importance_create_random_variable.py

--- feature_selection/feature_importance_create_random_variable.py
+++ feature_selection/feature_importance_create_random_variable.py
@@ -10,3 +10,34 @@ clf_fit = clf.fit(X, y)
 
 importance = clf_fit.feature_importances_
 importance
+
+
+
+# notebook notes
+# http://nbviewer.jupyter.org/github/donnemartin/data-science-ipython-notebooks/blob/master/analyses/churn.ipynb
+
+train_index,test_index = train_test_split(churn_df.index)
+
+forest = RF()
+forest_fit = forest.fit(X[train_index], y[train_index])
+forest_predictions = forest_fit.predict(X[test_index])
+
+importances = forest_fit.feature_importances_[:10]
+std = np.std([tree.feature_importances_ for tree in forest.estimators_],
+             axis=0)
+indices = np.argsort(importances)[::-1]
+
+# Print the feature ranking
+print("Feature ranking:")
+
+for f in range(10):
+    print("%d. %s (%f)" % (f + 1, features[f], importances[indices[f]]))
+
+# Plot the feature importances of the forest
+#import pylab as pl
+plt.figure()
+plt.title("Feature importances")
+plt.bar(range(10), importances[indices], yerr=std[indices], color="r", align="center")
+plt.xticks(range(10), indices)
+plt.xlim([-1, 10])
+plt.show()
